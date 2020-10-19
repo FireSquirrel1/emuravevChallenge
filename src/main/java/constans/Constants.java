41aa0df0866cb0e6b45659b41a1056cd385c52a5
evgenii muravev <emuravev@cinimex.ru>
2020-10-19 12:15:04 +0300

    no message

---
+++ src/main/java/constans/Constants.java
@@ -0,0 +1,11 @@
+package constans;
+
+public class Constants {
+    public static final String URL = "https://reqres.in/";
+    public static final String GET_PEOPLE_URL = "https://reqres.in/api/users?page=1";
+    public static final String POST_CREATE_USER_URL = "https://reqres.in/api/users";
+    public static final String USER_JSON = "{\n" +
+            "    \"name\": \"eugene\",\n" +
+            "    \"job\": \"tester\"\n" +
+            "}";
+}
